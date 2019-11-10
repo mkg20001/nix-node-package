4e2940c6874882e6829460f5122a7a9d9e1f764b
Maciej Krüger <mkg20001@gmail.com>
2019-11-10 20:51:47 +0100

    feat: drv

--- nix/node-package.nix
+++ nix/node-package.nix
@@ -2,7 +2,7 @@ let
   flatTree = deps: # TODO: filter out .dev == true
     map deps dep: {
       [dep.pname] = stdenv.mkDerivation {
-        pname = "npm-${dep.name}"; # TODO: split in pure and non-pure, so we can efficently re-use. non-pure would have node_modules, pure would just be specific pkg & version
+        pname = "npm-${dep.name}"; # TODO: split in pure and non-pure, so we can efficently re-use. non-pure would have node_modules, pure would just be specific pkg & version, dirty has node_modules links (dirty shouldn't be a package, instead it would just build all the links)
         version = dep.version;
         # TODO: inherit instead of whatever tf it does rn
         buildScript =
