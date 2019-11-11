9b9db6dbad4517e9b3354210ec9a186ac1c82c38
Maciej Krüger <mkg20001@gmail.com>
2019-11-11 15:14:27 +0100

    fix: syntax and comment out stuff

--- nix/node-package.nix
+++ nix/node-package.nix
@@ -1,26 +1,26 @@
 let
   flatTree = deps: # TODO: filter out .dev == true
-  let
-    integrity = builtins.match "^([a-z0-9]+)-(.+)$";
-  in
-    map deps dep: {
-      [dep.pname] = stdenv.mkDerivation {
-        pname = "npm-${dep.name}"; # TODO: split in pure and non-pure, so we can efficently re-use. non-pure would have node_modules, pure would just be specific pkg & version, dirty has node_modules links (dirty shouldn't be a package, instead it would just build all the links)
-        version = dep.version;
-        # TODO: inherit instead of whatever tf it does rn
-        buildScript =
-          ```
-          cp -rp package/ $out
-          mkdir $out/node_modules
-          ```
-          map ({key, value}:
-            ```
-            ln -s ${value} $out/node_modules/${key} # link derivation here
-            ```
-          ) flatTree(dep.dependencies)
-          # TODO: link bins/mans
-      }
-    }
+    let
+      integrity = builtins.match "^([a-z0-9]+)-(.+)$";
+    in
+      map (dep: {
+        #[dep.pname] = stdenv.mkDerivation {
+        #  pname = "npm-${dep.name}"; # TODO: split in pure and non-pure, so we can efficently re-use. non-pure would have node_modules, pure would just be specific pkg & version, dirty has node_modules links (dirty shouldn't be a package, instead it would just build all the links)
+        #  version = dep.version;
+        #  # TODO: inherit instead of whatever tf it does rn
+        #  buildScript =
+        #    ```
+        #    cp -rp package/ $out
+        #    mkdir $out/node_modules
+        #    ```
+        #    map ({key, value}:
+        #      ```
+        #      ln -s ${value} $out/node_modules/${key} # link derivation here
+        #      ```
+        #    ) flatTree(dep.dependencies)
+            # TODO: link bins/mans
+        #}
+      }) deps;
 in
   { root, pkgs }: attrs ? {}:
     with pkgs;
