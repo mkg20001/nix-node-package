0bcc73b7de67dde9dfa0fd6077c999c411aba3a7
Maciej Krüger <mkg20001@gmail.com>
2019-11-12 01:10:03 +0100

    kaboom

--- nix/node-package.nix
+++ nix/node-package.nix
@@ -1,32 +1,12 @@
 let
-  flatTree = deps: # TODO: filter out .dev == true
-    let
-      integrity = builtins.match "^([a-z0-9]+)-(.+)$";
-    in
-      map (dep: {
-        #[dep.pname] = stdenv.mkDerivation {
-        #  pname = "npm-${dep.name}"; # TODO: split in pure and non-pure, so we can efficently re-use. non-pure would have node_modules, pure would just be specific pkg & version, dirty has node_modules links (dirty shouldn't be a package, instead it would just build all the links)
-        #  version = dep.version;
-        #  # TODO: inherit instead of whatever tf it does rn
-        #  buildScript =
-        #    ```
-        #    cp -rp package/ $out
-        #    mkdir $out/node_modules
-        #    ```
-        #    map ({key, value}:
-        #      ```
-        #      ln -s ${value} $out/node_modules/${key} # link derivation here
-        #      ```
-        #    ) flatTree(dep.dependencies)
-            # TODO: link bins/mans
-        #}
-      }) deps;
+  d = {};
 in
   { root, lock, pkgs }: attrs: # TODO: get lock from "root + /package.lock.json"
     with pkgs;
     let
       json = builtins.fromJSON(builtins.readFile lock); # TODO: relative stuff, etc?
-      defaultAttrs = {
+    in
+      stdenv.mkDerivation ({
         name = json.name;
         version = json.version;
         sources = {
@@ -36,6 +16,4 @@ in
         buildScript = ''
           npm i -g --prefix=$out
           '';
-      };
-    in
-    stdenv.mkDerivation (defaultAttrs // attrs)
+      })
