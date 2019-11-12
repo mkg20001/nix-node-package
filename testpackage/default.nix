01c018ede8932fdcc2d6cfec81ac8493e636da70
Maciej Krüger <mkg20001@gmail.com>
2019-11-12 01:20:58 +0100

    feat: make it work a bit more

--- testpackage/default.nix
+++ testpackage/default.nix
@@ -4,15 +4,21 @@ let
   flatTree = { name, version, requires ? {}, dependencies ? [], resolved, integrity }:
     let
       integrity = builtins.match "^([a-z0-9]+)-(.+)$" integrity;
+
     in
       stdenv.mkDerivation({
         name = name;
         version = version;
 
       });
+  recurseTree = { dependencies, ... }:
+    builtins.concatMap(map (dep: {
+      ${dep.name} = flatTree(dep);
+    }) dependencies);
+  modules = recurseTree(json);
 in
   stdenv.mkDerivation({
     name = json.name;
     version = json.version;
-    src = "./";
+    src = "./src";
   })
