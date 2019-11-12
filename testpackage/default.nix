5cb9ca4911a344564ebbba6038be111b946e7ac9
Maciej Krüger <mkg20001@gmail.com>
2019-11-12 01:46:43 +0100

    kaboom

--- testpackage/default.nix
+++ testpackage/default.nix
@@ -18,11 +18,31 @@ let
 #      ${dep.name} = flatTree(dep);
 #    }) dependencies);
 #  modules = recurseTree(json);
+  recursePureEntry = { dependencies, ... }:
+    builtins.concatMap (map (dep: { ${dep.name} = makePureModule(dep); }) dependencies);
+  makePureModule = { name, version, requires ? {}, dependencies ? [], resolved, integrity }:
+    let
+      hash = builtins.match "^([a-z0-9]+)-(.+)$" integrity;
+      derive = {
+        ${dep.name} = stdenv.mkDerivation({
+          name = "npm-pure-${name}-${version}";
+          version = version;
+          src = fetchurl {
+            url = resolved;
+            ${hash[0]} = hash[1];
+          };
+        });
+      };
+    in
+      builtins.concatMap(builtins.concatLists([ [ derive ] recursePureEntry(dep) ]));
+
+  pureModules = recursePureEntry(json);
 in
   stdenv.mkDerivation({
     name = json.name;
     version = json.version;
     src = ./testpackage-0.0.1.tgz;
+    buildInputs = pureModules;
     installPhase = ''
       echo $pkgs
       '';
