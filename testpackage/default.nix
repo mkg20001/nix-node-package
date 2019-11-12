a37a0902649bc2ae57c5eb88dad88f7a66e2a08b
Maciej Krüger <mkg20001@gmail.com>
2019-11-12 01:30:34 +0100

    feat: disable stuff for now

--- testpackage/default.nix
+++ testpackage/default.nix
@@ -1,29 +1,29 @@
 with (import <nixpkgs> {});
 let
   json = builtins.fromJSON(builtins.readFile ./package-lock.json);
-  flatTree = { name, version, requires ? {}, dependencies ? [], resolved, integrity }:
-    let
-      hash = builtins.match "^([a-z0-9]+)-(.+)$" integrity;
-    in
-      stdenv.mkDerivation({
-        name = name;
-        version = version;
-        src = fetchurl {
-          url = resolved;
-          ${hash[0]} = hash[1];
-        };
-      });
-  recurseTree = { dependencies, ... }:
-    builtins.concatMap(map (dep: {
-      ${dep.name} = flatTree(dep);
-    }) dependencies);
-  modules = recurseTree(json);
+#  flatTree = { name, version, requires ? {}, dependencies ? [], resolved, integrity }:
+#    let
+#      hash = builtins.match "^([a-z0-9]+)-(.+)$" integrity;
+#    in
+#      stdenv.mkDerivation({
+#        name = name;
+#        version = version;
+#        src = fetchurl {
+#          url = resolved;
+#          ${hash[0]} = hash[1];
+#        };
+#      });
+#  recurseTree = { dependencies, ... }:
+#    builtins.concatMap(map (dep: {
+#      ${dep.name} = flatTree(dep);
+#    }) dependencies);
+#  modules = recurseTree(json);
 in
   stdenv.mkDerivation({
     name = json.name;
     version = json.version;
     src = ./testpackage-0.0.1.tgz;
-    buildScript = ''
-      echo test
+    installPhase = ''
+      echo $pkgs
       '';
   })
