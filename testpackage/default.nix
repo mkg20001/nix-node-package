b0846f9a033d9d4ea29be0f77d15e348c82c68cf
Maciej Krüger <mkg20001@gmail.com>
2019-11-22 23:29:45 +0100

    feat: dynamic fields

--- testpackage/default.nix
+++ testpackage/default.nix
@@ -1,12 +1,16 @@
 with (import <nixpkgs> {});
 let
+  root = ./.;
+
+  json = builtins.fromJSON(builtins.readFile "${root}/package-lock.json");
 in
   stdenv.mkDerivation({
-    name = "testpackage"; # TODO: dynamic
-    version = "0.0.1"; # TODO: dynamic
+    name = json.name; # TODO: dynamic
+    version = json.version; # TODO: dynamic
 
     src = ./.;
 
     installPhase = ''
+      ls
       '';
   })
