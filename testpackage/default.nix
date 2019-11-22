b30facaa4bcd25bdbabe546831160bbb423bdb14
Maciej Krüger <mkg20001@gmail.com>
2019-11-22 23:34:56 +0100

    feat: add dep

--- testpackage/default.nix
+++ testpackage/default.nix
@@ -11,7 +11,31 @@ let
         src = ./.;
 
         installPhase = ''
-          ls
+          getDepName() {
+            # TODO: add
+          }
+
+          installDep() {
+            # TODO: add
+          }
+
+          installDeps() {
+            local level="$1"
+
+            mkdir node_modules
+            pushd node_modules
+
+            for dep in ${deps[$level]}; do
+              getDepName $dep
+              installDep $dep $depName
+
+              pushd $depName
+              installDeps "$level/$depName"
+              popd
+            done
+
+            popd
+          }
           '';
       });
 in
